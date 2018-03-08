552ac039d0381f1696fa2c6b60e675c7fb37675b
Nicolas <nicolas.six@pau.fr>
2018-03-08 19:30:22 +0100

    change problems...

--- src/opt/test/OptimizationTest.java
+++ src/opt/test/OptimizationTest.java
@@ -490,7 +490,7 @@ public class OptimizationTest {
                 for (int j = 0; j < N.length; j++) {
                     for (int l = 0; l < num_runs; l++) {
                         new Analyze_Optimization_Test(
-                                "knapsack",
+                                "count_ones",
                                 algorithms[i],
                                 iterations[i],
                                 params,
@@ -502,6 +502,35 @@ public class OptimizationTest {
                     }
                 }
             }
-        } 
+        }
+
+        N = new int[]{50};
+        iterations = new int[]{5000, 5000, 5000, 5000};
+        for (int i = 0; i < algorithms.length; i++) {
+            for( Double[] param : param_map.get(algorithms[i])){
+                HashMap<String, Double> params = new HashMap<>();
+                params.put("SA_initial_temperature", param[0]);
+                params.put("SA_cooling_factor", param[1]);
+                params.put("GA_population", param[2]);
+                params.put("GA_mate_number", param[3]);
+                params.put("GA_mutate_number", param[4]);
+                params.put("MIMIC_samples", param[5]);
+                params.put("MIMIC_to_keep", param[6]);
+                for (int j = 0; j < N.length; j++) {
+                    for (int l = 0; l < num_runs; l++) {
+                        new Analyze_Optimization_Test(
+                                "nqueens",
+                                algorithms[i],
+                                iterations[i],
+                                params,
+                                N[j],
+                                N[j] / 5,
+                                other_params,
+                                l
+                        ).start();
+                    }
+                }
+            }
+        }
     }
 }
