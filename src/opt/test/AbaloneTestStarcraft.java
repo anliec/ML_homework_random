717be29b6cd56625f3772034782a94af34dab0be
Nicolas <nicolas.six@pau.fr>
2018-03-10 10:26:03 +0100

    select only 30 10 layer

--- src/opt/test/AbaloneTestStarcraft.java
+++ src/opt/test/AbaloneTestStarcraft.java
@@ -99,9 +99,9 @@ public class AbaloneTestStarcraft implements Runnable{
 //        hiddenLayerSizes.add(new Integer[] {10});
 //        hiddenLayerSizes.add(new Integer[] {15});
 //        hiddenLayerSizes.add(new Integer[] {25});
-        hiddenLayerSizes.add(new Integer[] {35});
+//        hiddenLayerSizes.add(new Integer[] {35});
 //        hiddenLayerSizes.add(new Integer[] {20, 10});
-//        hiddenLayerSizes.add(new Integer[] {30, 10});
+        hiddenLayerSizes.add(new Integer[] {30, 10});
 
 //        int count=0;
 
